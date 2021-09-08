89eeb671400092f2ab2a7c752df64e208a83ee70
korabhavitha <47246440+korabhavitha@users.noreply.github.com>
2021-09-08 10:17:55 +0530

    intial commit

---
+++ Protocol/message.py
@@ -0,0 +1,22 @@
+"""
+Author : Venkatesan Madappan
+Test Automation Framework
+"""
+
+
+class Messages:
+    """
+    Message for Communication with the Board
+    """
+    primitives = {
+        "ADVERTISE": "ADVERTISE",
+        "ADVERTISE_SUCCESSFUL": "ADVERTISE_SUCCESSFUL",
+        "CONNECT": "CONNECT",
+        "CONNECT_SUCCESSFUL": "CONNECT_SUCCESSFUL",
+        "DISCONNECT": "DISCONNECT",
+        "DISCONNECT_SUCCESSFUL": "DISCONNECT_SUCCESSFUL"
+        }
+
+    @staticmethod
+    def data_serialize(data):
+        return bytes(Messages.primitives[data]+'\r', 'utf-8')
